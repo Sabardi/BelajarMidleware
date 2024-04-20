fcee16036388dfae1fed1fd8235a45730669690f
Sabardi <Sabardibahari@gmail.com>
2024-04-20 10:08:32 +0800

    first commit

---
+++ database/migrations/2024_04_20_020231_update__to_posts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            //
+            $table->string('tanggal_lahir')->after('nama');
+            $table->string('tester')->after('nama');
+            $table->string('enpon');
+            $table->string('pepepe')->after('alamat');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            //
+        });
+    }
+};
